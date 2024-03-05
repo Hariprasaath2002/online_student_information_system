51bde69e044e15d48c1d6e6515173326fbfd2b04
Hariprasaath2002 <hariprasaath2002@gmail.com>
2024-03-05 13:40:19 +0530

    online_Student_information

---
+++ src/main/java/studentdetails/online_student_information_system/OnlineStudentInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package studentdetails.online_student_information_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudentInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStudentInformationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studentdetails/online_student_information_system/OnlineStudentInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package studentdetails.online_student_information_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStudentInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
